70d07faf7038f396c7636294db015d33a9eeb3c9
Miguel Cárdenas <101854059+MiguelMACD@users.noreply.github.com>
2023-12-11 05:53:06 -0500

    final

--- SpringBootTerceraNota/src/main/java/com/controller/Controlador.java
+++ SpringBootTerceraNota/src/main/java/com/controller/Controlador.java
@@ -1,28 +1,64 @@
 package com.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.interfaceService.InPersonaService;
+import org.springframework.web.bind.annotation.*;
 import com.model.Persona;
+import com.interfaceService.InPersonaService;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping
+@RequestMapping("/api/personas")
 public class Controlador {
 
-	@Autowired
-	private InPersonaService service;
-	
-	@GetMapping("/listar")
-	public String listar(Model model) {
-		List<Persona>personas=service.listar();
-		model.addAttribute("personas", personas);	
-		return "index";
-	}
-	
+    @Autowired
+    private InPersonaService service;
+
+    @GetMapping("/Listar")
+    public String listar(Model model) {
+        List<Persona> personas = service.listar();
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Persona> obtener(@PathVariable int id) {
+        Optional<Persona> persona = service.listarId(id);
+        return persona.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<Integer> crear(@RequestBody Persona persona) {
+        int id = service.save(persona);
+        return ResponseEntity.ok(id);
+    }
+
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<Void> actualizar(@PathVariable int id, @RequestBody Persona persona) {
+        Optional<Persona> personaExistente = service.listarId(id);
+        if (personaExistente.isPresent()) {
+            persona.setId(id);
+            service.save(persona);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable int id) {
+        Optional<Persona> personaExistente = service.listarId(id);
+        if (personaExistente.isPresent()) {
+            service.delete(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
